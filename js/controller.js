3844b2828acaf2ed9e05e7d0476077e2df0cd10d
Dino Stagno <dpaolostagno@gmail.com>
2023-02-25 04:03:24 -0300

    update

--- js/controller.js
+++ js/controller.js
@@ -9,12 +9,43 @@
 //   };
 //   getUsers();
 
-  (async = () => {
-    axios
-      .get("https://reqres.in/api/users")
-      .then((response) => {
-        const users = response.data.data;
-        console.log(`GET users`, users);
-      })
-      .catch((error) => console.error(error));
-  })();
+//   (async = () => {
+//     axios
+//       .get("https://reqres.in/api/users")
+//       .then((response) => {
+//         const users = response.data.data;
+//         console.log(`GET users`, users);
+//       })
+//       .catch((error) => console.error(error));
+//   })();
+
+const getAccounts = () => {
+    // axios
+    //   .post("https://dinostagno-dev-ed.my.salesforce.com/services/apexrest/AccountsV2/0012E00001ofxRyQAI")
+    //   .then((response) => {
+
+    //   })
+    //   .catch((error) => console.error());
+
+
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 00D2E000000mpVn!ARIAQHLertmUGnNzKfM472gtXin44RKeXnViqObmjmgXputuhlfqDcKrekadxwpIauWQfiqQQJmeu32aLwb.MTe3NHcEf6tb'
+      }
+      
+      axios.post("https://dinostagno-dev-ed.my.salesforce.com/services/apexrest/AccountsV2/0012E00001ofxRyQAI", data, {
+          headers: headers
+        })
+        .then((response) => {
+          dispatch({
+            type: FOUND_USER,
+            data: response.data[0]
+          })
+        })
+        .catch((error) => {
+          dispatch({
+            type: ERROR_FINDING_USER
+          })
+        })
+}
+getAccounts();
