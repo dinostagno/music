f030f1d27441c49c98fd76351ab3267e3812efbb
Dino Stagno <dpaolostagno@gmail.com>
2023-02-25 00:55:08 -0300

    update

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,17 @@
-(async () => {
-    const data = () => {
-        var theUrl = `https://climatologia.meteochile.gob.cl/application/productos/boletinClimatologicoDiario`;
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-        xmlHttp.send( null );
-        console.log(JSON.stringify(xmlHttp.responseText));
-        return xmlHttp.responseText;
+import axios from 'axios';
+
+axios.get("https://world.openfoodfacts.org/category/pastas/1.json")
+  .then(response => {
+    // access parsed JSON response data using response.data field
+    data = response.data
+    console.log(data.count)
+    console.log(data.products)
+  })
+  .catch(error => {
+    if (error.response) {
+      //get HTTP error code
+      console.log(error.reponse.status)
+    } else {
+      console.log(error.message)
     }
-    data();
-})();
+  })
