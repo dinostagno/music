65683d649f357a84feffd44bc0c19a67748aa6f0
Dino Stagno <dpaolostagno@gmail.com>
2023-02-25 01:32:00 -0300

    update

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,20 @@
-const getUsers = () => {
+// const getUsers = () => {
+//     axios
+//       .get("https://climatologia.meteochile.gob.cl/application/productos/boletinClimatologicoDiario")
+//       .then((response) => {
+//         const users = response;
+//         console.log(`GET users`, users);
+//       })
+//       .catch((error) => console.error(error));
+//   };
+//   getUsers();
+
+  (async = () => {
     axios
-      .get("https://climatologia.meteochile.gob.cl/application/productos/boletinClimatologicoDiario")
+      .get("https://reqres.in/api/users")
       .then((response) => {
         const users = response;
         console.log(`GET users`, users);
       })
       .catch((error) => console.error(error));
-  };
-  getUsers();
+  })();
