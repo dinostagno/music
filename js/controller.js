9559716988cb36527cdf0eb51a2a2c407a403f63
Dino Stagno <dpaolostagno@gmail.com>
2023-02-25 00:41:10 -0300

    update

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,11 @@
 (async () => {
-    setTimeout(() => {
-        console.log('hello')
-      
-    });
+    const data = () => {
+        var theUrl = `https://climatologia.meteochile.gob.cl/application/productos/boletinClimatologicoDiario`;
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        console.log(JSON.stringify(xmlHttp.responseText));
+        return xmlHttp.responseText;
+    }
+    data();
 })();
