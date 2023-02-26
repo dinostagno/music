1df3e69a25a6bc9f9b6e1e6873e5264edccc3202
Dino Stagno <dpaolostagno@gmail.com>
2023-02-26 00:06:10 -0300

    update

--- js/controller.js
+++ js/controller.js
@@ -71,15 +71,36 @@ getAccounts();
 //  }
 //  getToken();
 
-(async = () => {
-  const neural = new brain.NeuralNetwork();
-  const datos = [
-    {
-      "input": "¿que eres?",
-      "ouput": "soy una inteligencia artificial"
-    }
-  ];
-  neural.train(datos);
-  let respuesta = brain.likely("¿que eres?");
-  alert(respuesta);
-})();
+// (async = () => {
+//   const neural = new brain.NeuralNetwork();
+//   const datos = [
+//     {
+//       "input": "¿que eres?",
+//       "ouput": "soy una inteligencia artificial"
+//     }
+//   ];
+//   neural.train(datos);
+//   let respuesta = brain.likely("¿que eres?");
+//   alert(respuesta);
+// })();
+
+const machine = () => {
+  const network = new brain.NeuralNetwork();
+
+// Train the Network with 4 input objects
+network.train([
+  {input:[0,0], output:{zero:1}},
+  {input:[0,1], output:{one:1}},
+  {input:[1,0], output:{one:1}},
+  {input:[1,1], output:{zero:1}},
+]);
+
+// What is the expected output of [1,0]?
+let result = network.run([1,0]);
+console.log(result);
+
+// Display the probability for "zero" and "one"
+document.getElementById("demo").innerHTML =
+"one: " + result["one"] + "<br>" + "zero: " + result["zero"];
+}
+machine();
